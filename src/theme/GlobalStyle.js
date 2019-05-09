ab22d7643b993ef2ecece71c89fdf4870f3cce18
Sergey Ivashko <grayseneov@gmail.com>
2019-05-09 14:38:00 +0300

    fix overflow

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,13 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-   html, body, #root {
-    margin: 0;
-    padding: 0;
+   html, body {
     height: 100%;
     width: 100%;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
     font-family: 'Montserrat';
     font-size: 14px;
+  }
+  
+  #root {
+    height: 100%;
+    width: 100%;
+    overflow: auto;
   }
 
   a {
