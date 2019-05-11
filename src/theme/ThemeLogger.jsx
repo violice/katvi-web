4a354c03a45af13f9ce5d0426602c62f1ec9d592
Sergey Ivashko <grayseneov@gmail.com>
2019-05-11 15:04:47 +0300

    fix theme logger

--- src/theme/ThemeLogger.jsx
+++ src/theme/ThemeLogger.jsx
@@ -1,7 +1,9 @@
 import { withTheme } from 'styled-components';
 
 const ThemeLogger = withTheme(({ theme }) => {
-  console.log('%cTHEME', 'color:#7D4CDB', theme);
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('%cTHEME', 'color:#7D4CDB', theme);
+  }
   return null;
 });
 
