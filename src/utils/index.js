e740b1882ee5ae436d84e1fef757067b2e1d891d
Sergey Ivashko <grayseneov@gmail.com>
2019-05-21 22:49:48 +0300

    remove useless dependecies and utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,3 @@
 export { default as Api } from './api';
 export { default as useApi } from './useApi';
-export { default as injectReducer } from './injectReducer';
-export { default as injectSaga } from './injectSaga';
 export { default as loadable } from './loadable';
-// export { default as WithAccess } from './WithAccess';
-// export { default as WithAuth } from './WithAuth';
